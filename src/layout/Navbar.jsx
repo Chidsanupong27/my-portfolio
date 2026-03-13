be83de4d2f1ab13c64b890b38b67c57ed0df039c
Chidsanupong27 <Chidsan7@gmail.com>
2026-03-13 23:22:47 +0700

    31.10

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Button } from "../components/Button";
+
 const navLinks = [
   { href: "#about", label: "About" },
   { href: "#projects", label: "Projects" },
@@ -18,19 +20,25 @@ export const Navbar = () => {
 
         {/* { desktop nav} */}
         <div className="flex items-center gap-1">
-          <div className="glass">
+          <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
             {navLinks.map((link, index) => (
-              <a href={link.href} key={index}>
+              <a
+                href={link.href}
+                key={index}
+                className="px-4 py-2 text-sm text-muted-foreground  hover:text-foreground rounded-full hover:bg-surface"
+              >
                 {link.label}
               </a>
             ))}
           </div>
         </div>
 
+        {/* CTA Button */}
+
+        <div>
+          <Button size="sm">Contact ME</Button>
+        </div>
       </nav>
-
-
     </header>
-    
   );
 };

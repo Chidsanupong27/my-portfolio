ee7ee1bf6d3f602d3ea592c680388afe28103820
Chidsanupong27 <Chidsan7@gmail.com>
2026-03-13 16:54:29 +0700

    first core

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,36 @@
+const navLinks = [
+  { href: "#about", label: "About" },
+  { href: "#projects", label: "Projects" },
+  { href: "#experience", label: "Experience" },
+  { href: "#testimonials", label: "Testimonials" },
+];
+
+export const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+      <nav className="container mx-auto px-6 flex items-center justify-between ">
+        <a
+          href="#"
+          className="text-xl font-bold tracking-tight hover:text-primary"
+        >
+          TOP<span className="text-primary">PO</span>
+        </a>
+
+        {/* { desktop nav} */}
+        <div className="flex items-center gap-1">
+          <div className="glass">
+            {navLinks.map((link, index) => (
+              <a href={link.href} key={index}>
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </div>
+
+      </nav>
+
+
+    </header>
+    
+  );
+};

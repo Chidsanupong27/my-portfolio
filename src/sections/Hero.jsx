c51d68eb637df7b309a5a5b61845e5cf4b489d12
Chidsanupong27 <Chidsan7@gmail.com>
2026-03-14 17:17:26 +0700

    ep2

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,10 @@
 export const Hero = () => {
-    return <section></section>
-}
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+        {/* bg */}
+        <div>
+            <img src="" alt="" />
+        </div>
+    </section>
+  );
+};
